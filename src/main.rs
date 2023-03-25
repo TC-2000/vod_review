4437dd4207c905e2ff21931bbd4f3c906246a450
rholdman <thatholdman@gmail.com>
2023-03-25 12:45:21 -0400

    slowly progressing through sqlite integration. Need to fix problem with use of undefined cargo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let connection = sqlite::open(":memory:").unwrap();   
+
+    let connection = sqlite::open(":memory:").unwrap();
 
     let query = "
     CREATE TABLE videos (videoname TEXT, characterone TEXT, charactertwo TEXT, notes TEXT);
@@ -7,4 +8,15 @@ fn main() {
     INSERT INTO videos VALUES ('Testvideoname', 'testcharacterone', 'testcharactertwo', 'notes');
     "; 
     connection.execute(query).unwrap(); 
+
+    let query = "SELECT videos WHERE characterone == characterone"; 
+
+    connection 
+        .iterate(query, |pairs| {
+            for &(characterone, value) in pairs.iter() {
+                println!("{} = {}", characterone, value.unwrap()); 
+            }
+            true
+        })
+        .unwrap(); 
 }
