ff7a37d1891e5249a0b50f1687d42b1f9291fb79
rholdman <thatholdman@gmail.com>
2023-03-25 09:56:59 -0400

    Added initial sqlite functionality. Need to resolve compiler errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open(":memory:").unwrap();   
+
+    let query = "
+    CREATE TABLE videos (videoname TEXT, characterone TEXT, charactertwo TEXT, notes TEXT);
+    INSERT INTO videos VALUES ('Testvideoname', 'testcharacterone', 'testcharactertwo', 'notes');
+    INSERT INTO videos VALUES ('Testvideoname', 'testcharacterone', 'testcharactertwo', 'notes');
+    "; 
+    connection.execute(query).unwrap(); 
 }
